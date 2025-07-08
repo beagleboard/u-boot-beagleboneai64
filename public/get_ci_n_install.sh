d7ddda5e19583f2791594f777eed117561f489b9
Robert Nelson <robertcnelson@gmail.com>
2025-07-08 17:05:20 -0500

    TI_FIRMWARE: 11.01.05, OPTEE: 4.7.0-rc1 gitlab runner
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ public/get_ci_n_install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit
+fi
+
+if [ -f ./sysfw.itb ] ; then
+	rm -rf ./sysfw.itb || true
+fi
+
+if [ -f ./tiboot3.bin ] ; then
+	rm -rf ./tiboot3.bin || true
+fi
+
+if [ -f ./tispl.bin ] ; then
+	rm -rf ./tispl.bin || true
+fi
+
+if [ -f ./u-boot.img ] ; then
+	rm -rf ./u-boot.img || true
+fi
+
+wget https://beagle-pkgs.gitlab.io/u-boot-beagleboneai64/sysfw.itb
+wget https://beagle-pkgs.gitlab.io/u-boot-beagleboneai64/tiboot3.bin
+wget https://beagle-pkgs.gitlab.io/u-boot-beagleboneai64/tiboot3.bin
+wget https://beagle-pkgs.gitlab.io/u-boot-beagleboneai64/u-boot.img
+
+if [ -d /boot/firmware/ ] ; then
+	cp -v ./sysfw.itb /boot/firmware/
+	cp -v ./tiboot3.bin /boot/firmware/
+	cp -v ./tispl.bin /boot/firmware/
+	cp -v ./u-boot.img /boot/firmware/
+	sync
+fi
+
+if [ -d /boot/efi/ ] ; then
+	cp -v ./sysfw.itb /boot/efi/
+	cp -v ./tiboot3.bin /boot/efi/
+	cp -v ./tispl.bin /boot/efi/
+	cp -v ./u-boot.img /boot/efi/
+	sync
+fi
+
+if [ -b /dev/mmcblk0 ] ; then
+	mmc bootpart enable 1 2 /dev/mmcblk0
+	mmc bootbus set single_backward x1 x8 /dev/mmcblk0
+	mmc hwreset enable /dev/mmcblk0
+
+	echo "Clearing eMMC boot0"
+
+	echo '0' >> /sys/class/block/mmcblk0boot0/force_ro
+
+	echo "dd if=/dev/zero of=/dev/mmcblk0boot0 count=32 bs=128k"
+	sudo dd if=/dev/zero of=/dev/mmcblk0boot0 count=32 bs=128k
+
+	echo "dd if=/boot/firmware/tiboot3.bin of=/dev/mmcblk0boot0 bs=128k"
+	sudo dd if=/boot/firmware/tiboot3.bin of=/dev/mmcblk0boot0 bs=128k
+	sync
+fi
+
+#
