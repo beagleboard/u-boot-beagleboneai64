7c1245b7573c7724393b86a31619cb97e1273aab
Robert Nelson <robertcnelson@gmail.com>
2026-09-10 19:23:03 -0500

    v2026.07-Beagle-12.01.00.06 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- public/get_n_install.sh
+++ public/get_n_install.sh
@@ -21,10 +21,10 @@ if [ -f ./u-boot.img ] ; then
 	rm -rf ./u-boot.img || true
 fi
 
-wget https://github.com/beagleboard/u-boot-beagleboneai64/releases/download/v2026.04-Beagle-11.02.18/sysfw.itb
-wget https://github.com/beagleboard/u-boot-beagleboneai64/releases/download/v2026.04-Beagle-11.02.18/tiboot3.bin
-wget https://github.com/beagleboard/u-boot-beagleboneai64/releases/download/v2026.04-Beagle-11.02.18/tispl.bin
-wget https://github.com/beagleboard/u-boot-beagleboneai64/releases/download/v2026.04-Beagle-11.02.18/u-boot.img
+wget https://github.com/beagleboard/u-boot-beagleboneai64/releases/download/v2026.07-Beagle-12.01.00.06/sysfw.itb
+wget https://github.com/beagleboard/u-boot-beagleboneai64/releases/download/v2026.07-Beagle-12.01.00.06/tiboot3.bin
+wget https://github.com/beagleboard/u-boot-beagleboneai64/releases/download/v2026.07-Beagle-12.01.00.06/tispl.bin
+wget https://github.com/beagleboard/u-boot-beagleboneai64/releases/download/v2026.07-Beagle-12.01.00.06/u-boot.img
 
 sync
 
